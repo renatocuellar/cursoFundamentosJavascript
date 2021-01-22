d9e1bb21cc505d1d844238774210f9512b66937b
renatocuellar <matealva@gmail.com>
2021-01-21 23:21:24 -0500

    Se agrega un glosario jun

---
+++ varibles.js
@@ -0,0 +1,11 @@
+// Siempre que creamos un avariable y no le asignamos valkor se crea con el valor por defecto undefined
+
+var nombre 
+var apellido
+
+// Desde ES6 se comenzo a usar más la comilla simple que la doble, no hay mucha diferencia
+
+ // JS ya no es obligatorio poner el punto y coma al final, es opcional, pero ya no se usa mucho, también se puede usar en una sola línea
+apellido = 'Cuéllar', nombre = 'Renato'
+
+console.log('Hola '+nombre +' '+ apellido)
